768181c7de169d114ae5c349ae159397771ebad5
Milan Bhanuka <115621782+MilanBhanuka@users.noreply.github.com>
2023-07-13 05:20:08 +0100

    Tutorial 02
    1. Write a function in Scala that takes a string as input and returns the reverse of the string using a recursive approach.
    
    2. Write a function in Scala that takes a list of strings as input and returns a new list containing only the strings that have a length greater than 5.
    
    3. You are given two integers, num1 and num2. Write a function in Scala to determine the arithmetic mean (average) of the two numbers, rounded to two decimal places. Return the result as a floating-point number.

---
+++ Tutorial003/Q01.scala
@@ -0,0 +1,17 @@
+object Q01 {
+  def main(args: Array[String]): Unit = {
+    print("Input a string : ")
+    var str = scala.io.StdIn.readLine()
+    var reverseStr =reverse(str)
+    print(reverseStr)
+  }
+
+  def reverse( str:String):String = {
+    if(str.isEmpty){
+      ""
+    }
+    else{
+      reverse(str.tail)+str.head
+    }
+  }
+}
